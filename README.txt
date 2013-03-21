b3bbd35c16b9596ce6112f52c9d373a0978b15b2
Daniel Holth <dholth@fastmail.fm>
2013-03-21 18:46:22 -0400

    update documentation

--- README.txt
+++ README.txt
@@ -3,4 +3,14 @@ jkf
 
 Prototype JSON Key Fingerprints implementation.
 
-JSON Key Fingerprints is a specification for hashing JSON Web Key.
+JSON Key Fingerprints is a method for hashing a JSON Web Key. The
+resulting value, called a fingerprint, can be recomputed from another
+representation of the same key and used as a short identifier.
+
+JSON Web Keys do not currently have a standard fingerprinting scheme. This
+scheme is not based on any standard but is very straightforward. The
+implementation takes the non-metadata members of a JWK (just the public
+values used by the crypto math, and not the "kid" or "use" flags), sorts
+them, and outputs UTF-8 encoded JSON with no whitespace separators. The
+hash of that binary string is the JSON Key Fingerprint.
+
--- jkf.py
+++ jkf.py
@@ -1,11 +1,13 @@
-# JSON Key Fingerprints prototype
-# (not based on any specification)
+# JSON Key Fingerprints (JKF) prototype.
+# (Not based on any standard.)
 
 import hashlib
 import json
 from collections import OrderedDict
 
-HASHABLE = {
+HASHABLE = { 
+        # key type : set(fields) mapping. JKF only hashes the public
+        # non-metadata fields from the JSON Web Key.
         "RSA":{'kty', 'n', 'e'},
         "EC":{'kty', 'crv', 'x', 'y'},
         "Ed25519":{'kty', 'vk'}
@@ -16,11 +18,11 @@ def normalize(jwk):
     return OrderedDict(sorted((k, jwk[k]) for k in HASHABLE[jwk['kty']]))
 
 def dumps(jwk):
-    """Return JSON without whitespace as bytes."""
+    """Return JSON without whitespace as utf-8 encoded bytes."""
     return json.dumps(jwk, separators=(",", ":")).encode('utf-8')
 
 def fingerprint(jwk, algorithm="sha256"):
     """Return the fingerprint of the given JSON Web Key as a hashlib object."""
-    h = hashlib.new(algorithm)    
+    h = hashlib.new(algorithm)
     h.update(dumps(normalize(jwk)))
     return h
